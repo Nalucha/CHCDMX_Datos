7c3a866b4b1485948f6aa37068edda8e742648fc
rodAlfaro-98 <rodrigoalfarod@gmail.com>
2022-06-23 19:07:11 -0500

    Se agregaron las medianas de NOX y CO con sus respectivos scripts

---
+++ NOX.py
@@ -0,0 +1,50 @@
+import pandas as pd
+
+Promedios = {}
+
+meses = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre']
+
+for i in range(2010,2022):
+    dir = str(i)+"/NOX/"
+
+    año = {}
+
+    print(i)
+
+    for j in range(12):
+
+        mes = []
+
+        print(j)
+
+        file_name = "Promedio horarios de nox"
+        if(j == 0):
+            file_name += ".csv"
+        else:
+            file_name += "("+str(j)+").csv"
+        df = pd.read_csv(dir+file_name)
+
+        for k in ["HGM", "MER", "CAM"]:
+            if(k in df.keys()):
+                print(k)
+
+                mes.append(df[k].median())
+
+        año[meses[j]] = mes
+
+
+    #print(año)
+    df = pd.DataFrame(año, index = [["HGM","MER","CAM"]])
+
+    df = df.transpose()
+    print(df)
+
+    #Promedios[i] = df
+
+    file_path = "mediana_NOX_"+str(i)+".csv"
+
+    df.to_csv(file_path)
+
+    
+
+
